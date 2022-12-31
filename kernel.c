cf3b5387b4e4713c5907a0fb8c3e3cb69c88b759
Fariz Muradov <aze2201@gmail.com>
2022-12-31 21:37:58 +0100

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,11 +1,9 @@
 #include "kernel.h"
 
-
 uint16 vga_input(unsigned char ch, uint8 input_case_color, uint8 background_color) 
 {
   uint16 ax = 0;
   uint8 ah = 0, al = 0;
-
   ah = background_color;
   ah <<= 4;
   ah |= input_case_color;
@@ -13,9 +11,16 @@ uint16 vga_input(unsigned char ch, uint8 input_case_color, uint8 background_colo
   ax <<= 8;
   al = ch;
   ax |= al;
-
   return ax;
 }
+
+//init buffer for vga
+void init_vga(uint8 input_case_color, uint8 background_color)
+{
+  buff_vga = (uint16*)vga_addr;  //point buff_vga pointer to vga_addr 
+  clear_buff(&buff_vga, input_case_color, background_color);  //clear buffer
+}
+
 
 void clear_buff(uint16 **buffer, uint8 input_case_color, uint8 background_color)
 {
@@ -25,12 +30,6 @@ void clear_buff(uint16 **buffer, uint8 input_case_color, uint8 background_color)
   }
 }
 
-//init buffer for vga
-void init_vga(uint8 input_case_color, uint8 background_color)
-{
-  buff_vga = (uint16*)vga_addr;  //point buff_vga pointer to vga_addr 
-  clear_buff(&buff_vga, input_case_color, background_color);  //clear buffer
-}
 
 void kernel_main()
 {
